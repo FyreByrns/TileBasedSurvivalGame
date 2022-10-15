2fef65b18cdf85df24db390e2e03b84e104db788
Gavin White <horace5413@gmail.com>
2022-10-15 09:37:46 -0700

    add Begin to scene proto

--- UPnPTest/Scenes/Scene.cs
+++ UPnPTest/Scenes/Scene.cs
@@ -5,6 +5,7 @@ namespace TileBasedSurvivalGame.Scenes {
     abstract class Scene {
         public abstract string Name { get; }
         public abstract Scene Next { get; protected set; }
+        public abstract void Begin(Engine instance);
         public abstract void Update(Engine instance, float elapsed);
         public abstract void Tick(Engine instance);
         public abstract void Render(Engine instance);
