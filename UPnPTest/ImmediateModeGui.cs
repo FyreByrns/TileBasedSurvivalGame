1f988fe4b2510c873dbcab7ac9b44364fd4516b1
Gavin White <horace5413@gmail.com>
2022-10-16 15:17:26 -0700

    add dropdown that works with arbitrary enums

--- UPnPTest/ImmediateModeGui.cs
+++ UPnPTest/ImmediateModeGui.cs
@@ -1,4 +1,5 @@
 ﻿using PixelEngine;
+using System;
 
 //// = documentation
 // = per-step working comments
@@ -39,6 +40,38 @@ namespace TileBasedSurvivalGame {
                     return true;
                 }
             }
+            return false;
+        }
+
+        public static bool EnumDropdown<T>(Engine context, int x, int y, T currentSelection, out T result, ref bool hovered, int margin = 2, int scale = 1) {
+            int textPixelSize = 8 * scale;
+            int width = 0;
+            int height = textPixelSize + margin * 2;
+
+            T[] possibilities = (T[])Enum.GetValues(typeof(T));
+            string[] names = new string[possibilities.Length];
+            for (int i = 0; i < possibilities.Length; i++) {
+                names[i] = possibilities[i].ToString();
+
+                width = Math.Max(width, names[i].Length * textPixelSize + margin * 2);
+            }
+
+            // if the mouse is hovering over the dropdown, show all possibilities
+            hovered |= context.MouseX > x && context.MouseY > y && context.MouseX < x + width && context.MouseY < y + height;
+            if (hovered) {
+                height = (names.Length + 1) * (textPixelSize + margin * 2);
+                hovered = context.MouseX > x && context.MouseY > y && context.MouseX < x + width && context.MouseY < y + height;
+                for (int i = 0; i < names.Length; i++) {
+                    if (
+                    Button(context, x, y + (textPixelSize + margin * 2) * (i + 1), names[i], width, 0, margin, scale)) {
+                        result = possibilities[i];
+                        return true;
+                    }
+                }
+            }
+            // otherwise, just show the current selection
+            Button(context, x, y, names[Array.IndexOf(possibilities, currentSelection)], width, 0, margin, scale);
+            result = currentSelection;
             return false;
         }
     }
